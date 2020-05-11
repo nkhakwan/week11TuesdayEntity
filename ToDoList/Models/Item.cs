24272aa21aecd7215819c9de618a0ba4c659d056
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-11 17:16:52 -0600

    finish

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> { };
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public Item(string des)
+    {
+      Description = des;
+      _instances.Add(this);
+    }
 
   }
 }
