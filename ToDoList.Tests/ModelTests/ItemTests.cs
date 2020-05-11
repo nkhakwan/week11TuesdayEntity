eaec59dc62fd5210744eafdd367c58378068752a
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-11 16:48:08 -0600

    add setter

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,8 +6,40 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTests
   {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
 
-    // Test methods will go here.
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.Description;
+      //Assert
+      Assert.AreEqual(description, result);
 
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "walk the dog";
+      Item newItem = new Item(description);
+
+      //act
+      string updatedDescription = "do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
   }
 }
