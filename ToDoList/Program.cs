a0f29b7047f201f1f7ca19f5085fdeea46d13975
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-18 22:26:26 -0600

    add functionality

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,81 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    public static string UserInput(string response)
+    public static void Main(string[] args)
     {
-      if (response == "a")
-      {
-        Console.Write("Enter your item:");
-        string res = Console.ReadLine();
-        int i;
-        Console.Write("Priority Level:");
-        bool resp = int.TryParse(Console.ReadLine(), out i);
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-        if (resp)
-        {
-          Item thisItem = new Item(res, i);
-        }
-        else
-        {
-          Item thisItem = new Item(res);
-        }
-
-
-
-        Console.Write("[V]iew, [A]dd, or [Q]uit:");
-        response = Console.ReadLine().ToLower();
-
-      }
-      else if (response == "v")
-      {
-        List<Item> tempItem = Item.GetAll();
-        if (tempItem.Count == 0)
-        {
-          Console.WriteLine("Your list is empty ya dingus!");
-          response = "a";
-        }
-        else
-        {
-          foreach (Item item in tempItem)
-          {
-            Console.WriteLine(item.Description);
-          }
-
-          Console.Write("[V]iew, [A]dd, or [Q]uit:");
-          response = Console.ReadLine().ToLower();
-        }
-      }
-
-      return response;
-
-    }
-    public static void Main()
-    {
-      Console.WriteLine("Hi, welcome to your to do list");
-
-      Console.Write(@"Would you like to [V]iew or [A]dd to your list? -- ");
-
-      string response = Console.ReadLine().ToLower();
-
-      Console.WriteLine(response);
-
-      while (response != "a" && response != "v")
-      {
-        Console.WriteLine("Sorry, I didn't catch that!");
-        Console.WriteLine("Please type 'a' to add or 'v' to view");
-        Console.Write("Would you like to [V]iew or [A]dd to your list? -- ");
-        response = Console.ReadLine().ToLower();
-      }
-      while (response != "q")
-      {
-        response = UserInput(response);
-      }
-      Console.WriteLine("Bye!");
+      host.Run();
     }
   }
 }
