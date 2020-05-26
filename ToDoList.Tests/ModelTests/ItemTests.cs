9eb71900f6ea1cffcc7ef377f87779264190b611
nkhakwan <post_khan@yahoo.com>
2020-05-26 10:32:32 -0700

    committing all the files learned in the weekend and todays lesson so far

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -14,6 +14,64 @@ namespace ToDoList.Tests
       Item.ClearAll();
     }
 
+    public ItemTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
+    }
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //Arrange
+      Item testItem = new Item("Mow the lawn");
+
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    {
+      // Arrange, Act
+      Item firstItem = new Item("Mow the lawn");
+      Item secondItem = new Item("Mow the lawn");
+
+      // Assert
+      Assert.AreEqual(firstItem, secondItem);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
+    {
+      //Arrange
+      Item newItem = new Item("Mow the lawn");
+      newItem.Save();
+      Item newItem2 = new Item("Wash dishes");
+      newItem2.Save();
+
+      //Act
+      Item foundItem = Item.Find(newItem.Id);
+      //Assert
+      Assert.AreEqual(newItem, foundItem);
+    }
+
+    [TestMethod]
+public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
+    {
+      //Arrange
+      List<Item> newList = new List<Item> { };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -62,19 +120,24 @@ namespace ToDoList.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
+    
+
     [TestMethod]
-    public void GetAll_ReturnItems_ItemList()
+    public void GetAll_ReturnsItems_ItemList()
     {
       //Arrange
       string description01 = "Walk the dog";
-      string description02 = "Walk the dishes";
+      string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
+      newItem1.Save(); // New code
       Item newItem2 = new Item(description02);
+      newItem2.Save(); // New code
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-      //Act 
+      //Act
       List<Item> result = Item.GetAll();
-      //assert
+
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
